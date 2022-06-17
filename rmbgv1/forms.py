a70ec20ff94def9741c001a9122515a6c100956c
MG7 <mukhlisg7@gmail.com>
2022-06-17 23:27:29 +0500

    first commit final

--- rmbgv1/forms.py
+++ rmbgv1/forms.py
@@ -1,10 +1,9 @@
-
 # forms.py
 from django import forms
 from .models import *
-  
+
+
 class ImgProcForm(forms.ModelForm):
-  
     class Meta:
         model = ImgToProc
         fields = ['Image_To_Process']
--- rmbgv1/views.py
+++ rmbgv1/views.py
@@ -10,8 +10,6 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 import requests
 import json
 
-from rembg import remove
-import transfer
 
 # Create your views here.
 
@@ -19,61 +17,45 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-  
 # Create your views here.
 def proc_image_view(request):
-  
     if request.method == 'POST':
         form = ImgProcForm(request.POST, request.FILES)
-  
+
         if form.is_valid():
-
             img_obj = form.instance
-            img_file_path = os.getcwd() + "\\media\\images\\" + str(img_obj.Image_To_Process)
+
+            print(form.files['Image_To_Process'])
+            rmbgv1_api = "https://rmbgv1api.herokuapp.com/upload_image2proc"
+            values = {'filename': str(form.files['Image_To_Process'])}
+            file = {'file1': form.files['Image_To_Process'].read()}
+
+            req_resp = requests.post(rmbgv1_api, files=file, data=values)
             
-            print("img_filename : " + str(img_obj.Image_To_Process))
+            print(req_resp.text)
+            json_data = json.loads(req_resp.text)
+            print(json_data.get('message'))
+            up_file_link = json_data.get('up_file_link')
+            request.session['up_file_link'] = up_file_link
 
-            #
+            return redirect('success')
 
-            print(img_file_path)
-            form.save()
-            if os.path.isfile(img_file_path):
-                print("Current Dir = "+ os.getcwd())
+        else:
+            form = ImgProcForm()
+            return render(request, 'uploadImg.html', {'form': form})
 
-                #print("Last File Name : " + str(img_obj.Image_To_Process).split("/")[-1])
-
-                img_file_rmd_path =  os.getcwd() + "\\media\\images\\bg_removed\\" +str(img_obj.Image_To_Process).split("/")[-1]
-                img_file_rmd_path_final = img_file_rmd_path.replace(img_file_rmd_path[len(img_file_rmd_path) - 3:], "png")
-                print("Image remomve path: " + img_file_rmd_path_final)
-
-                img_removed_path = removeBG(str(img_file_path), img_file_rmd_path_final)
-                print(img_file_rmd_path_final)
-
-                img_url = "https://transfer.sh/" + str(img_obj.Image_To_Process).split("/")[-1]
-
-                response = requests.put(img_url , data=open(img_file_rmd_path_final,'rb').read()) 
-                    
-                up_file_link = response.content.decode("utf-8")
-                request.session['up_file_link'] = up_file_link
-
-                return redirect('success')
-
-            else:
-                form = ImgProcForm()
-                return render(request, 'uploadImg.html', {'form' : form})
     else:
         form = ImgProcForm()
-    return render(request, 'uploadImg.html', {'form' : form})
-  
-  
+        return render(request, 'uploadImg.html', {'form': form})
+
+
 def success(request):
     if 'up_file_link' in request.session:
         up_file_link = request.session['up_file_link']
-        return HttpResponse('successfully uploaded <br> <a href="'+ up_file_link + '">Download Now</a>')
+        return HttpResponse('successfully uploaded <br> <a href="' + up_file_link + '">Download Now</a>')
     else:
         form = ImgProcForm()
-        return render(request, 'uploadImg.html', {'form' : form})        
-
+        return render(request, 'uploadImg.html', {'form': form})
 
 
 def removeBG(input_path, output_path):
@@ -83,21 +65,3 @@ def removeBG(input_path, output_path):
             output = remove(input)
             o.write(output)
             return output
-
-
-
-def uploadToGofile(file_path):
-    mp_encoder = MultipartEncoder(
-        fields={
-            'filesUploaded': (file_path, open(file_path, 'rb'))
-        }
-    )
-    r = requests.post(
-        'https://store1.gofile.io/upload',
-        data=mp_encoder,
-        headers={'Content-Type': mp_encoder.content_type}
-    )
-    scrap = r.json()
-    print(r)
-    # send "Token file" 123Abc
-    Token = scrap['data']['code']
